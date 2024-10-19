624e518fcdd7e2963daf345312e9debed71bf29a
threader <michael.heltne@gmail.com>
2024-10-19 02:36:31 +0200

    linux: hardened: fix more old hardening patches, i should have read https://github.com/GrapheneOS/kernel_g

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1548,7 +1548,8 @@ static void prep_new_page(struct page *page, unsigned int order, gfp_t gfp_flags
 {
 	post_alloc_hook(page, order, gfp_flags);
 
-	if (IS_ENABLED(CONFIG_PAGE_SANITIZE_VERIFY)) {
+	if (IS_ENABLED(CONFIG_PAGE_SANITIZE_VERIFY) && want_init_on_free()) {
+		int i;
 		for (i = 0; i < (1 << order); i++)
 			verify_zero_highpage(page + i);
 	}
