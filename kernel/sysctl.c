bcfe69aae5eba92f0bf786a01ead4684b251cc43
threader <michael.heltne@gmail.com>
2024-10-20 07:47:54 +0200

    merge residue, these functions have been moved to debugfs and now seem safe?

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -139,25 +139,6 @@ static enum sysctl_writes_mode sysctl_writes_strict = SYSCTL_WRITES_STRICT;
 int sysctl_legacy_va_layout;
 #endif
 
-<<<<<<< HEAD
-=======
-#ifdef CONFIG_SCHED_DEBUG
-static int min_sched_granularity_ns __read_only = 100000;		/* 100 usecs */
-static int max_sched_granularity_ns __read_only = NSEC_PER_SEC;	/* 1 second */
-static int min_wakeup_granularity_ns __read_only;			/* 0 usecs */
-static int max_wakeup_granularity_ns __read_only = NSEC_PER_SEC;	/* 1 second */
-#ifdef CONFIG_SMP
-static int min_sched_tunable_scaling __read_only = SCHED_TUNABLESCALING_NONE;
-static int max_sched_tunable_scaling __read_only = SCHED_TUNABLESCALING_END-1;
-#endif /* CONFIG_SMP */
-#endif /* CONFIG_SCHED_DEBUG */
-
-#ifdef CONFIG_COMPACTION
-static int min_extfrag_threshold __read_only;
-static int max_extfrag_threshold __read_only = 1000;
-#endif
-
->>>>>>> 47615581453c (make sysctl constants read-only)
 #endif /* CONFIG_SYSCTL */
 
 /*
