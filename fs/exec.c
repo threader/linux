aa798edd6b6b89723a227a3c5163bf1ac5933265
threader <michael.heltne@gmail.com>
2024-10-20 09:06:25 +0200

    observed while applying the grapheneos kernel gs patches, certainly upset the canary in fs

--- fs/exec.c
+++ fs/exec.c
@@ -294,7 +294,7 @@ static int __bprm_mm_init(struct linux_binprm *bprm)
 	mm->stack_vm = mm->total_vm = 1;
 	mmap_write_unlock(mm);
 	bprm->p = vma->vm_end - sizeof(void *);
-	if (randomize_va_space)
+	if (!(current->personality & ADDR_NO_RANDOMIZE) && randomize_va_space)
 		bprm->p ^= get_random_u32() & ~PAGE_MASK;
 	return 0;
 err:
